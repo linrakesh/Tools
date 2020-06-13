5deca34b4c6ac0d3a3a5de0bb88a383c1cc49fab
rakesh <rakesh.linux@gmail.com>
2020-06-13 09:50:28 +0530

    drop down menu added

--- passwordtools/views.py
+++ passwordtools/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+import random
+
+
 def password_generator(request):
-    return render(request,'passwordtools/password_gen.html')
+    if request.method == 'POST':
+        length = int(request.POST['length'])
+        chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+}{"
+        password = ""
+        for i in range(length+1):
+            password += random.choice(chars)
+        print(password)
+        return render(request, 'passwordtools/password_gen.html', {'password': password})
+    else:
+        return render(request, 'passwordtools/password_gen.html')
