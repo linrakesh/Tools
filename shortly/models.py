a3abf97152114696ae2738fb72e472a00679db0a
rakesh <rakesh.linux@gmail.com>
2020-06-22 18:37:26 +0530

    Two new tools added

--- shortly/models.py
+++ shortly/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here
 
 
-class shorturl(class (models.Model):
-    original_url=models.URLField(max_length=250)
-    short_url=models.URLField(max_length=50)
+class shorturl(models.Model):
+    original_url = models.URLField(max_length=250)
+    short_url = models.URLField(max_length=50)
