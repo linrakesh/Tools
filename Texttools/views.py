1427d3c2ac4a40a96f28bf714fe037c375c64f07
rakesh kumar <rakesh@binarynote.com>
2021-01-29 12:35:25 +0530

    word count

--- Texttools/views.py
+++ Texttools/views.py
@@ -7,10 +7,9 @@ def word_count(request):
   if request.method=='POST':
     data = request.POST['text']
     n = len(data)
+    list1 = data.split()
     words = len(data.split())
-    vowels=0;
-    conso=0
-    upper=lower=digit=0
+    vowels=conso=upper=lower=digit=medium =large=short=0
     for x in data: 
       if x in 'AEIOUaeiou':
         vowels +=1
@@ -22,7 +21,20 @@ def word_count(request):
         lower+=1  
       elif x.isdigit():
         digit+=1
-    result ={'chars':n, 'words': words,'vowels':vowels,'conso':conso,'upper':upper,'lower':lower,'digit':digit}
+    for x in list1:
+        if len(x)<=4:
+           short+=1
+        if len(x)>=6 and len(x)<=10:
+           medium+=1
+        if len(x)>10:
+           large+=1
+    uppercase = data.upper()
+    lowercase = data.lower()
+
+    result ={'chars':n, 'words': words,'vowels':vowels,'conso':conso,'upper':upper,'lower':lower,
+             'digit':digit,'short':short,'medium':medium,'large':large,'uppercase':uppercase,'lowercase':lowercase,
+            }
+    
     return render(request, 'Texttools/word_count.html',{'summery':result})
   else:
     return render(request, 'Texttools/word_count.html')
